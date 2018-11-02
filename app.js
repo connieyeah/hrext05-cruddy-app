baacddd340cbc672ee22af9ef4856e4e2825224a
Connie Ea <connieyeah@gmail.com>
2018-11-02 01:34:48 -0700

    refactor code

--- app.js
+++ app.js
@@ -1,70 +1,109 @@
-/*
-After localStorage is cleared -> go to create new playlist
-
-Edit playlist name -> edit key in localStorage
-
-*/
-
-// Create a request variable and assign a new XMLHttpRequest object to it.
+// Globals
 var request = new XMLHttpRequest();
-var keyValue; //key for localStorage
-var playlist = {}; //property to be stored in localStorage
+var playlistName;
+var playlist;
 
-var getSongsByArtist = function() {
-  // Clear tracklist div
-  document.getElementById('trackList').innerHTML = "";
+//to format key for localStorage
+function sanitizeKey(key){
+  return key.replace(/[^a-zA-Z0-9]/g, '');
+}
 
-  // Get searched artist value in input field typed in by user
-  const searchedArtist = document.getElementById('artist').value
+// Creates img element used in track row div
+function createTrackImg(currTrack) {
+  var imgElement = document.createElement("img")
+  imgElement.setAttribute("src", currTrack.image[1]['#text']);
+  return imgElement
+}
 
-  // Uppercase first letter of artist name
-  const caseFormattedSearch = searchedArtist.charAt(0).toUpperCase() + searchedArtist.slice(1)
+function createTrackRow(currTrack, userInput) {
+  var validId = currTrack.mbid
+  // Data can possibly return a track with a MISSING MBID, so check for MBID
+  if (validId) {
+    var trackRow = $("<div id='trackDiv' data=" + currTrack.mbid + "></div>")
+    var trackImg = createTrackImg(currTrack)
+    trackRow.append(trackImg)
 
-  // Open a new connection, using the GET request on the URL endpoint
-  request.open('GET', 'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=' + caseFormattedSearch + '&api_key=01d67fa8b4f33dce213f0c366ed33511&format=json', true);
-  
-  request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  console.log(`What is the data returned from the API? : `, data)
+    var trackText = document.createTextNode(currTrack.artist.name + ' - ' + currTrack.name) 
+    trackRow.append(trackText)
 
-  data.toptracks.track.forEach(function(currTrack){
-    if(!currTrack.mbid) {
-      return 'song not available to add';
-    }
+    var addBtn = document.createElement("button")
+    addBtn.innerText = 'Add Song';
+    $(addBtn).on("click", function() {
+      var userInput = window.localStorage.getItem('playlistName')
+      var playlist = JSON.parse(window.localStorage.getItem(userInput))
+      console.log(playlist)
 
-      var node = $("<div id='trackDiv' data=" + currTrack.mbid + "></div>")
-      node.data(`dude`)
-      // Create image element and append to div
-      var imgElement = document.createElement("img")
-      imgElement.setAttribute("src", currTrack.image[1]['#text']);
-      node.append(imgElement)
-      // Create artist text and append to div
-      var text = document.createTextNode(caseFormattedSearch + ' - ' + currTrack.name) 
-      node.append(text)
-      var addBtn = document.createElement("button")
-      addBtn.innerText = 'Add Song';
-      // click event to add songs to playlist & save to localStorage
-      $(addBtn).on("click", function() {
+      if (playlist) {
         if (!playlist[currTrack.mbid]) {
-          playlist[currTrack.mbid] = {songArtist: currTrack.artist.name, songName: currTrack.name, mbid: currTrack.mbid}
-          console.log(`current playlist`, playlist)
-          localStorage.setItem(keyValue, JSON.stringify(playlist));
+           playlist[currTrack.mbid] = {songArtist: currTrack.artist.name, songName: currTrack.name, mbid: currTrack.mbid}
+          localStorage.setItem(userInput, JSON.stringify(playlist));
         } else {
           alert('song already exists in playlist')
         }
-      })
-
-      node.append(addBtn)
-      $("#trackList").append(node)
+      }
     })
+    trackRow.append(addBtn)
+    $("#trackList").append(trackRow)
   }
-  // Send request
-  request.send(); 
+}
+
+// Makes a request to API to get back artist data
+function getSongsByArtist() {
+  // Clear track list every time we search for a new artist
+  document.getElementById('trackList').innerHTML = "";
+  // Get the artist name from inout field
+  const searchedArtist = document.getElementById('artist').value
+  const sanitizedSearch = searchedArtist.charAt(0).toUpperCase() + searchedArtist.slice(1)
+  // Opens the request to be made to the API
+  request.open('GET', 'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=' + sanitizedSearch + '&api_key=01d67fa8b4f33dce213f0c366ed33511&format=json', true);
+  // When the request is made, map over the data and create a row from each track
+  request.onload = function() {
+    var apiData = JSON.parse(this.response);
+    console.log(`What is the data returned from the API? : `, apiData)
+    apiData.toptracks.track.forEach(function(currTrack) {
+      createTrackRow(currTrack)
+    });
+  }
+
+  request.send()
+}
+
+// Deletes a track from saved playlist
+function deleteTrack(key) {
+  var playlistName = localStorage.getItem('playlistName')
+  var playlist = JSON.parse(localStorage.getItem(playlistName));
+  if (playlist[key]) {
+    delete playlist[key];
+  }
+  //updates changes in localStorage
+  localStorage.setItem(playlistName, JSON.stringify(playlist))
+  //updates collapsible playlist
+  $("#"+key).remove()
 }
 
 
 $(document).ready(function(){
+  document.getElementById("artist").addEventListener("keyup", function(event){
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      getSongsByArtist()
+    }
+  });
+  document.getElementById("titlePlaylist").addEventListener("keyup", function(event){
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      var userInput = sanitizeKey($('#titlePlaylist').val());
+      // Set title of page to what user inputs
+      $h1.text($('#titlePlaylist').val())
+      // Create new playlist in localStorage with initial {}
+      localStorage.setItem(userInput, JSON.stringify({}))
+      localStorage.setItem('playlistName', userInput)
+      $forTitle.hide()
+      $forSearch.show()
+      $forTracks.show()
+      $forPlaylist.show()
+    }
+  });
 
   var $forTitle = $('.forTitle');
   var $forSearch = $('.forSearch');
@@ -73,8 +112,9 @@ $(document).ready(function(){
   var $body = $('body');
   var $h1 = $('h1');
 
+  var userInput = sanitizeKey($('#titlePlaylist').val());
 
-  if(window.localStorage.length === 0){
+  if(!userInput){
     $forTitle.show()
     $forSearch.hide()
     $forTracks.hide()
@@ -83,45 +123,23 @@ $(document).ready(function(){
     $forTitle.hide()
   }
 
-  // console.log(window.localStorage)
-
-
-//to format key for localStorage
-function createKey(key){
-  return key.replace(/[^a-zA-Z0-9]/g, '');
-}
-
-
-$('#titleBtn').on("click", function(){
-  // if (!$('#titlePlaylist').val()) {
-  //   alert('Enter playlist title!')
-  // } else {
-    keyValue = createKey($('#titlePlaylist').val());
+  $('#titleBtn').on("click", function() {
+    var userInput = sanitizeKey($('#titlePlaylist').val());
+    // Set title of page to what user inputs
     $h1.text($('#titlePlaylist').val())
-  // }
-  localStorage.setItem(keyValue, JSON.stringify(playlist))
-  $forTitle.empty()
-  $forSearch.show()
-  $forTracks.show()
-  $forPlaylist.show()
-})
+    // Create new playlist in localStorage with initial {}
+    localStorage.setItem(userInput, JSON.stringify({}))
+    localStorage.setItem('playlistName', userInput)
 
-  // Not the best way to do this, but this code adds a listener so when you press enter key (keyCode - 13),
-  //  it executes the function that the 'Search' button executes. 
-  document.getElementById("artist")
-      .addEventListener("keyup", function(event){
-      // Cancel the default action, if needed
-    event.preventDefault();
-      if (event.keyCode === 13) {
-          getSongsByArtist()
-      }
-  });
+    $forTitle.hide()
+    $forSearch.show()
+    $forTracks.show()
+    $forPlaylist.show()
+  })
 
-  //code for collapsible
+  // Create collapsable
   var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
+  for (var i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
@@ -133,85 +151,55 @@ $('#titleBtn').on("click", function(){
     });
   }
 
-// console.log(`this is localStorage:`,window.localStorage)
 
-// to display current playlist
-$(".collapsible").on("click", function() {
-  //clear so code beneath does not keep appending to repeating localStorage
-  $(".show-playlist").empty();
+  // to display current playlist
+  $(".collapsible").on("click", function() {
+    //clear so code beneath does not keep appending to repeating localStorage
+    $(".show-playlist").empty();
+    var playlistName = localStorage.getItem('playlistName')
+    var list = JSON.parse(localStorage.getItem(playlistName));
 
-  var list;
-
-  list = JSON.parse(localStorage.getItem(keyValue));
-
-  var $myPlaylist = $('<div class="playlist">');
-    
-  //defined before it's called or else function undefined due to hoisting (es5)
-  function toDelete(key) {
-    console.log(`ATTEMPTING TO DELETE: `, key)
-    // console.log(`before playlist`, playlist)
-    if (playlist[key]) {
-      delete playlist[key];
+    var $myPlaylist = $('<div class="playlist">');
+    console.log(`is this running: `, list)
+      
+    //populate collapsible
+    for (var key in list) {
+      var artistSong = list[key];
+      console.log(`unique mbid: `,artistSong.mbid)
+      //parent element
+      var $playlistRow = $("<div class='playlist-row' id=" + artistSong.mbid + " data=" + artistSong.mbid + ">")
+      var $songItem = $('<div><span class="songArtist">' + artistSong.songArtist + ' - </span>' +
+        '<span class="songName">' + artistSong.songName + '</span></div>')
+      
+      //create delete button to append per row
+      var deleteBtn = $("<button id=" + artistSong.mbid + " type='button'>Delete</button>")
+      
+      $(deleteBtn).on("click", function(e){
+        var songId = e.target.id
+        deleteTrack(songId)
+      })
+      $songItem.append(deleteBtn)
+      $playlistRow.append($songItem)  
+      $myPlaylist.append($playlistRow);
     }
-    //updates changes in localStorage
-    localStorage.setItem(keyValue, JSON.stringify(playlist))
-    //updates collapsible playlist
-    $("#"+key).remove()
-    // console.log(`after playlist`, playlist)
-  }
 
-  //populate collapsible
-  for (var key in list) {
-    var artistSong = list[key];
-    console.log(`unique mbid: `,artistSong.mbid)
-    //parent element
-    var $playlistRow = $("<div class='playlist-row' id=" + artistSong.mbid + " data=" + artistSong.mbid + ">")
-    var $songItem = $('<div><span class="songArtist">' + artistSong.songArtist + ' - </span>' +
-      '<span class="songName">' + artistSong.songName + '</span></div>')
-    
-    //create delete button to append per row
-    var deleteBtn = $("<button id=" + artistSong.mbid + " type='button'>Delete</button>")
-    
-    $(deleteBtn).on("click", function(e){
-      var songId = e.target.id
-      toDelete(songId)
-    })
+    $(".show-playlist").append($myPlaylist)  
+  });
 
-    $songItem.append(deleteBtn)
-    $playlistRow.append($songItem)  
-    $myPlaylist.append($playlistRow);
-  }
-
-  $(".show-playlist").append($myPlaylist)  
-
-});
-
-
-
-
-// $("#formButton").click(function(){
-//         $("#form1").toggle();
-//     });
-
-// //rename key in localStorage
-// $("#rename-playlist-btn").on("click", function(){
-//   $("#rename-key").toggle();
-//     var newKey = $(this).data
-// });
-
-// $(".clear-all-songs").on("click", function() {
-//   alert('Are you sure you want to delete all songs from playlist?')
-//   localStorage.removeItem(keyValue)
-//   $(".show-playlist").empty();
-// })
-
-
-//clears localStorage (playlist)
-$(".clear-cache-btn").on("click", function(){
-// alert('Are you sure you want to delete playlist?')
-  // localStorage.removeItem(keyValue)
-  localStorage.clear();
-  $(".show-playlist").empty(); //not only child elements, but also any text within the set of matched elements
-});
+  // Clears localStorage (playlist)
+  $(".clear-cache-btn").on("click", function(){
+    var playlistName = localStorage.getItem('playlistName')
+    localStorage.removeItem(playlistName)
+    localStorage.removeItem('playlistName')
+    $('#artist').val('')
+    $('#titlePlaylist').val('')
+    $forSearch.hide()
+    $forTracks.hide()
+    $('#trackList').empty()
+    $forPlaylist.hide()
+    $forTitle.show()
+    $h1.text('');
+    $(".show-playlist").empty(); //not only child elements, but also any text within the set of matched elements
+  });
 
 });
